0cbbbc4a11b635187a89c3d2af4326f2415c9bc8
Mário Gomes <marioferreiragomes333@gmail.com>
2026-04-21 14:58:18 +0100

    feature:business rule finalized

--- GoodHamburger/apps/api/src/GoodHamburger.Domain/Entities/OrderItem.cs
+++ GoodHamburger/apps/api/src/GoodHamburger.Domain/Entities/OrderItem.cs
@@ -1,4 +1,5 @@
-﻿using GoodHamburger.Domain.Exceptions;
+﻿using GoodHamburger.Domain.Enum;
+using GoodHamburger.Domain.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GoodHamburger.Domain.Entities; 
-public class OrderItem: EntityBase {
+public class OrderItem {
+    public Guid Id { get; set; }
     public Guid OrderId { get; set; }
     public Guid MenuId { get; set; }
     public int Qtd { get; private set; }
@@ -23,6 +25,28 @@ public class OrderItem: EntityBase {
         MenuId = menuId;
         Qtd = 1;                      
         UnitPrice = unitPrice;
+        Id = Guid.NewGuid();
+    }
+
+    public void AddSideDish(Guid sideDishesId, SideDishCategory category, decimal unitPrice) {
+      
+        var alreadyHasCategory = _OrderSideDishes.Any(s => s.Category == category);
+        
+        if (alreadyHasCategory) {
+            var nome = category == SideDishCategory.FRIES ? "batata frita" : "refrigerante";
+            throw new DomainException(
+                $"Este pedido já contém um {nome}. Não é permitido duplicar acompanhamentos.");
+        }
+
+        _OrderSideDishes.Add(new OrderSideDishes(sideDishesId, category, unitPrice));
+    }
+
+    public bool HasFries() => _OrderSideDishes.Any(s => s.Category == SideDishCategory.FRIES);
+    public bool HasDrink() => _OrderSideDishes.Any(s => s.Category == SideDishCategory.DRINK);
+
+    public decimal CalculateTotal() {
+        var sideDishesTotal = _OrderSideDishes.Sum(sd => sd.CalculateTotal());
+        return (UnitPrice * Qtd) + sideDishesTotal;
     }
 
 }
--- GoodHamburger/apps/api/src/GoodHamburger.Domain/Entities/OrderSideDishes.cs
+++ GoodHamburger/apps/api/src/GoodHamburger.Domain/Entities/OrderSideDishes.cs
@@ -2,7 +2,8 @@
 using GoodHamburger.Domain.Exceptions;
 
 namespace GoodHamburger.Domain.Entities; 
-public class OrderSideDishes: EntityBase {
+public class OrderSideDishes {
+    public Guid Id { get; set; }
     public Guid OrderItemId { get; set; }
     public Guid SideDishesId { get; set; }
     public int Qtd { get; set; }
@@ -14,10 +15,14 @@ public class OrderSideDishes: EntityBase {
     {}
 
     public OrderSideDishes(Guid sideDishesId, SideDishCategory category, decimal unitPrice) {
+        
         if (unitPrice < 0) throw new DomainException("Preço não pode ser negativo.");
+
         SideDishesId = sideDishesId;
         Category = category;
         Qtd = 1;                    
         UnitPrice = unitPrice;
+        Id=Guid.NewGuid();
     }
+    public decimal CalculateTotal() => UnitPrice * Qtd;
 }
